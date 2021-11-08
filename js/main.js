64b2985d61ce519ca1a7ad86698eb863fc521dc1
Doroshenko_st6 <lightestwaymusic@gmail.com>
2021-11-08 16:14:56 +0200

    new

---
+++ js/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const login = document.querySelector('#login');
+
+login.onclick = function() {
+    let login = prompt('login:');
+
+    if(login == 'admin') {
+    
+      let password = prompt('password:');
+    
+      if(password == 'admin') {
+        alert(`Hello, ${login} !!!`);
+      } else if ( password == null ) {
+        alert('Otmena');
+      } else if (password == '') {
+        alert('PUSTO')
+      }
+      else {
+        alert('Ne verNO!!!!!');
+      }
+      
+    } else if (login == '' || null) {
+      alert('Otmena');
+    } else {
+      alert('error');
+    }
+
+  };
+
+
+  $('.location-slider').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    variableWidth: true
+  });
