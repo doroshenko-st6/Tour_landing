b846e43287b40f644059bb1d5b5bf729e629703a
Doroshenko_st6 <lightestwaymusic@gmail.com>
2022-01-04 17:38:28 +0200

    new

--- js/main.js
+++ js/main.js
@@ -1,32 +1,46 @@
 'use strict';
 
-const login = document.querySelector('#login');
+// const login = document.querySelector('#login');
 
-login.onclick = function() {
-    let login = prompt('login:');
+// login.onclick = function() {
+//     let login = prompt('login:');
 
-    if(login == 'admin') {
+//     if(login == 'admin') {
     
-      let password = prompt('password:');
+//       let password = prompt('password:');
     
-      if(password == 'admin') {
-        alert(`Hello, ${login} !!!`);
-      } else if ( password == null ) {
-        alert('Otmena');
-      } else if (password == '') {
-        alert('PUSTO')
-      }
-      else {
-        alert('Ne verNO!!!!!');
-      }
+//       if(password == 'admin') {
+//         alert(`Hello, ${login} !!!`);
+//       } else if ( password == null ) {
+//         alert('Otmena');
+//       } else if (password == '') {
+//         alert('PUSTO')
+//       }
+//       else {
+//         alert('Ne verNO!!!!!');
+//       }
       
-    } else if (login == '' || null) {
-      alert('Otmena');
-    } else {
-      alert('error');
-    }
+//     } else if (login == '' || null) {
+//       alert('Otmena');
+//     } else {
+//       alert('error');
+//     }
+//   };
 
-  };
+
+$(document).ready(function() {
+  $('.header__burger').click(function(event) {
+    $('.header__burger, .header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
+
+$(document).ready(function() {
+  $('.header__link').click(function(event) {
+    $('.header__burger, .header__menu').removeClass('active');
+    $('body').removeClass('lock');
+  });
+});
 
 
   $('.location-slider').slick({
